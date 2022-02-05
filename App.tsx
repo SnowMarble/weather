bd7eeccaca0f8c2a0c5edde9de8a4349ab0548c6
kraccoon-dev <kraccoon0420@gmail.com>
2022-02-05 21:51:11 +0900

    toggle darkmode demo

--- App.tsx
+++ App.tsx
@@ -1,71 +1,51 @@
-import React from "react";
+import React from 'react'
 import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
   NativeBaseProvider,
-  extendTheme,
-  VStack,
-  Code,
-} from "native-base";
-import NativeBaseIcon from "./components/NativeBaseIcon";
+  Center,
+  Text,
+  Button,
+  useColorMode,
+  useColorModeValue,
+} from 'native-base'
 
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
+interface Props {
+  children: React.ReactNode
+}
 
-// extend the theme
-export const theme = extendTheme({ config });
+function SimpleCard() {
+  const { toggleColorMode } = useColorMode()
+  return (
+    <Center w="80%" bg={useColorModeValue("light.100", "light.700")} rounded="xl" shadow="5" py="10">
+      <Text
+        fontSize="2xl"
+        fontWeight="bold"
+        color={useColorModeValue('light.700', 'light.100')}>
+        Hi There! 🖐️
+      </Text>
+      <Button bg="blue.400" mt="5" onPress={toggleColorMode}>
+        <Text color="blue.50" fontSize="md">
+          Toggle Dark Mode
+        </Text>
+      </Button>
+    </Center>
+  )
+}
+
+function Background({children}: Props) {
+  return (
+    <Center
+      flex={1}
+      bg={useColorModeValue('light.100', 'light.800')}
+      safeArea>{children}</Center>
+  )
+}
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Code>App.tsx</Code>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
+      <Background>
+        <SimpleCard />
+      </Background>
     </NativeBaseProvider>
-  );
-}
-
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light" ? true : false}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
+  )
 }
